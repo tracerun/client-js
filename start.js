d4084238856b8a7c6d588dae5a63c4efccd16688
drkaka <drkaka@drkaka.com>
2017-05-04 17:46:26 +0800

    Add get actions, get targets, get slots methods.

--- start.js
+++ start.js
@@ -6,9 +6,21 @@ client.setErrFunc(err => {
   console.log(err);
 });
 
-let metaClient = new client.Client();
-metaClient.getMeta(meta => {
-  console.log(meta);
+let cli = new client.Client();
+cli.getMeta(meta => {
+  console.log("meta:", meta);
+});
+
+cli.getActions(actions => {
+  console.log("actions:", actions);
+});
+
+cli.getTargets(targets => {
+  console.log("targets:", targets);
+});
+
+cli.getSlots("c:\\Users\\drkaka\\Desktop\\Untitled-1.txt", 0, 0, slots => {
+  console.log("slots:", slots);
 });
 
 // agent.start(ok => {
